8c616a31c2351b04596347abab16707c08745056
Zeyusss <dght839@gmail.com>
2025-05-14 13:44:14 +0300

    Restructured project & finalized full Auth flow (login, register, forgot/reset password, 2FA, lock screen, confirm email) + completed all error pages (401, 403, 404, 500)

---
+++ assets/js/errors/errors.js
@@ -0,0 +1,7 @@
+    lottie.loadAnimation({
+      container: document.getElementById('lottie-500'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'https://assets10.lottiefiles.com/packages/lf20_j3UXNf.json' 
+    });
